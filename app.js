bac432f3512bc750084b2d95b9b21a17f88e80be
Manish Jha <manishjhazyx@gmail.com>
2021-09-22 13:37:14 +0530

    added functionality

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput =document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" ;
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
+function getTranslationURL(text){
+    return  serverURL + "?" + "text=" + text 
+}
 
+function errorHandler(error){
+    console.log("error occurred", error);
+    alert("something wrong with server, try after some time");
+}
 
 function clickHandler() {
-        outputDiv.innerText = "translated: " + txtInput.value;
+        // outputDiv.innerText = "translated: " + txtInput.value;
+        var inputText = txtInput.value;
+        
+        fetch(getTranslationURL(inputText))
+            .then(response => response.json())
+            .then(json =>{
+                var translatedText = json.contents.translated;
+                outputDiv.innerText = translatedText;
+            })
+            .catch(errorHandler)
+        
+        
 };
  btnTranslate.addEventListener("click", clickHandler);
